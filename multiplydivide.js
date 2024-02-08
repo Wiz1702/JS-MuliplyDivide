7716cdabebb87081f6c852105344af43c9ba7729
James Skön <skonjp@kenyon.edu>
2024-02-08 15:42:02 -0500

    Update multiplydivide.js

--- multiplydivide.js
+++ multiplydivide.js
@@ -3,6 +3,11 @@ function multiplyBy()
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
         document.getElementById("result").innerHTML = num1 * num2;
+        if (num1 * num2 > 1000) {
+                document.getElementById("message").innerHTML = "BIG!!!"; 
+        } else {
+                document.getElementById("message").innerHTML = "";
+        }
 }
 
 function divideBy() 
